bc2c641159fdba7ad31f81498c8161333d721490
yigithanyigit <yigit@Yigithans-MacBook-Pro.local>
2024-02-09 18:08:50 +0300

    Multiplication and Division is supported right now

--- interpreter.py
+++ interpreter.py
@@ -7,6 +7,9 @@ TYPE_MINUS = 'MINUS'
 TYPE_DIV = 'DIV'
 TYPE_MUL = 'MUL'
 
+TYPE_LPAREN = 'LPAREN'
+TYPE_RPAREN = 'RPAREN'
+
 TYPE_EOF = 'EOF'
 TYPE_WHITESPACE = 'WHITESPACE'
 
@@ -25,8 +28,9 @@ class Interpreter:
     def __init__(self, text) -> None:
         self.text = text 
         self.pos = 0
-        self.current_token = None
         self.current_char = text[self.pos]
+        self.current_token = self.get_next_token()
+        self.op_list = (TYPE_PLUS, TYPE_MINUS, TYPE_DIV, TYPE_MUL)
 
     def error(self) -> None:
         raise Exception('Error While Parsing input')
@@ -58,7 +62,7 @@ class Interpreter:
         while self.current_char is not None:
         
             
-            if self.current_char == ' ':
+            if self.current_char.isspace():
                 self.skip_whitespace()
                 continue
 
@@ -77,6 +81,28 @@ class Interpreter:
                 self.advance()
                 return token
 
+            if self.current_char == '*':
+                token = Token(TYPE_MUL, None)
+                self.advance()
+                return token
+
+            if self.current_char == '/':
+                token = Token(TYPE_DIV, None)
+                self.advance()
+                return token
+
+            if self.current_char == '(':
+                token = Token(TYPE_LPAREN, None)
+                self.advance()
+                return token
+
+
+            if self.current_char == ')':
+                token = Token(TYPE_RPAREN, None)
+                self.advance()
+                return token
+
+ 
             self.error()
 
         return Token(TYPE_EOF, None)
@@ -89,33 +115,43 @@ class Interpreter:
         else:
             self.error()
 
+    def factor(self):
+        token = self.current_token
+        if self.current_token.type == TYPE_INTEGER:
+            self.eat(TYPE_INTEGER)
+            return token.value
+        elif self.current_token.type == TYPE_LPAREN:
+            self.eat(TYPE_LPAREN)
+            result = self.expr()
+            self.eat(TYPE_RPAREN)
+            return result
+
+
+    def term(self):
+        result = self.factor()
+        while self.current_token is not None and self.current_token.type in (TYPE_MUL, TYPE_DIV):
+            if self.current_token.type == TYPE_MUL:
+                self.eat(TYPE_MUL)
+                result *= self.factor()
+            elif self.current_token.type == TYPE_DIV:
+                self.eat(TYPE_DIV)
+                result /= self.factor()
+ 
+        return result
+
 
     def expr(self):
-        result = Token(TYPE_INTEGER, 0)
-        
-        self.current_token = self.get_next_token()
-        while self.current_token.type is not TYPE_EOF:
-            
-            left = result if result.value != 0 else self.current_token
-            self.eat(TYPE_INTEGER)
 
-            op = self.current_token
-
-            if op.type == TYPE_PLUS:
+        result = self.term()
+        while self.current_token.type in self.op_list:
+            if self.current_token.type == TYPE_PLUS:
                 self.eat(TYPE_PLUS)
-            elif op.type == TYPE_MINUS:
+                result += self.term()
+            elif self.current_token.type == TYPE_MINUS:
                 self.eat(TYPE_MINUS)
+                result -= self.term()
 
-            right = self.current_token
-            #self.eat(TYPE_INTEGER)
-
-            if op.type == TYPE_PLUS:
-                result.value = left.value + right.value
-            elif op.type == TYPE_MINUS:
-                result.value = left.value - right.value
-
-        return result.value
-
+        return result
 
           
     
